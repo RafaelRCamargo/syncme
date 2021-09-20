cc110a9f58af239382467e85d5a10ba24bfa99b0
RafaelRCamargo <rafaelrakochinski@gmail.com>
2021-09-19 22:16:13 -0300

    first commit

--- src/pages/SyncMobile.jsx
+++ src/pages/SyncMobile.jsx
@@ -8,8 +8,14 @@ export default function SyncMobile() {
 
   return (
     <div className="container-home">
+      {rootId && mobileId ? 
+      <>
       <p className="subtitle-home">Root: <strong>{rootId}</strong></p>
       <p className="subtitle-home">Mobile: <strong>{mobileId}</strong></p>
+      </>
+      :
+      <p className="subtitle-home"><i>Loading...</i></p> 
+      }
       <a className="footer-copyrights" href="https://github.com/RafaelRCamargo">© 1900-2021 Rafael R. Camargo</a>
     </div>
   )
